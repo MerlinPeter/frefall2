adc0717710299cd9bb5a36321ac614dc117edf32
MerlinPeter <merlinahila@gmail.com>
2017-01-02 10:32:03 -0800

    first change

--- frefall2/GameScene.m
+++ frefall2/GameScene.m
@@ -1,45 +1,46 @@
 //
 //  GameScene.m
-//  frefall2
+//  freefall
 //
-//  Created by Peter on 1/2/17.
+//  Created by Peter on 1/1/17.
 //  Copyright (c) 2017 peter. All rights reserved.
 //
 
 #import "GameScene.h"
 
+
 @implementation GameScene
 
 -(void)didMoveToView:(SKView *)view {
+    
+    self.physicsWorld.contactDelegate = self;
+    self.physicsWorld.gravity = CGVectorMake(0.2f, -7.8f);
     /* Setup your scene here */
-    SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+    self.name = @"Fence";
+    self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
     
-    myLabel.text = @"Hello, World!";
-    myLabel.fontSize = 45;
-    myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                   CGRectGetMidY(self.frame));
-    
-    [self addChild:myLabel];
+    SKSpriteNode *fireball = [SKSpriteNode spriteNodeWithImageNamed:@"fireball.png"];
+    fireball.name = @"fireball";
+    fireball.position = CGPointMake(10, 600);
+    fireball.zPosition = 1;
+    fireball.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:fireball.size.width/2];
+    fireball.physicsBody.dynamic = YES;
+    //fireball.position = CGPointMake(100, self.size.height/2);
+    fireball.physicsBody.friction = 0.0;
+    fireball.physicsBody.restitution = 0.9;
+    fireball.physicsBody.linearDamping = 0.0;
+    fireball.physicsBody.angularDamping = 0.0;
+    fireball.physicsBody.allowsRotation = NO;
+    fireball.physicsBody.mass = 1.0;
+    //fireball.physicsBody.velocity = CGVectorMake(150, 300.0);
+    fireball.physicsBody.affectedByGravity = YES;
+    fireball.physicsBody.usesPreciseCollisionDetection = YES;
+    [self addChild:fireball];
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
     
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.xScale = 0.5;
-        sprite.yScale = 0.5;
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
-    }
 }
 
 -(void)update:(CFTimeInterval)currentTime {
